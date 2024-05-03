1c55c8d42f5b24974b8f0bea2da315c1fd575160
Ahmad Majed <ahmadmajed@Ahmads-MacBook-Air.local>
2024-05-03 17:35:21 +0300

    Initial Commit

---
+++ routes/contactUsRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { Router } from "express";
+import { contactUs } from "../controllers/contactUsController.js";
+
+const router = Router();
+
+router.post("/contact", contactUs);
+
+export default router;
