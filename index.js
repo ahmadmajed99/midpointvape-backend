bef62b7aac2b398cd70e2b124b05059904fe1fcc
Ahmad Majed <ahmadmajed@Ahmads-MacBook-Air.local>
2024-05-03 19:04:03 +0300

    test

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
   res.send("API is running ...");
 });
 
+app.get("/home", (req, res) => {
+  res.send("Hello World!");
+});
+
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 // });
